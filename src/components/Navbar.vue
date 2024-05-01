293edf1482f7c17153d195f78947a1ac629f740d
Jordi Osarenkhoe Petro <jordiosarenkhoe@outlook.es>
2024-05-01 18:48:26 +0200

    move to device

---
+++ src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="sticky top-0 z-10 w-full h-12 p-2 border border-gray-400 border-opacity-25">
+        <div class="flex items-center justify-between">
+            <div class="text-lg font-bold">Boilerplate</div>
+            <div class="flex space-x-4">
+                <router-link to="/" class="hover:text-blue-500">Home</router-link>
+                <router-link to="/about" class="hover:text-blue-500">About</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="grid w-full h-full min-h-full grid-cols-7 grid-rows-5 gap-4 p-2">
+    <div class="col-span-2 row-span-5 bg-gray-400 rounded">
+      1
+    </div>
+    <div class="col-span-5 col-start-3 bg-gray-400 rounded">
+      2
+    </div>
+    <div class="col-span-5 col-start-3 row-span-4 row-start-2 bg-gray-400 rounded">
+      3
+    </div>
+  </div>
 </template>
